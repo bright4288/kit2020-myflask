93180b2ed265d258711bd82138779b604191037b
강유진 <alsl4288@naver.com>
2020-06-01 17:43:00 +0900

    복구하기

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, url_for, abort
+from flask import Flask, request, render_template, redirect, url_for, abort, session
+
 app = Flask(__name__)
 
 
@@ -46,37 +47,22 @@ def input_num(num):
         return "없어요"
     #return 'Hello, {}!'.format(name)
 
-#로그인
+app.secreat_key = b'aaa!111/'
 
-@app.route('/login',methods = ['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
-        id = request.form['id']
-        pw = request.form['pw']
-        print(id,type(id))
-        print(pw,type(pw))
-        #id와 pw가 임의로 정한 값이랑 비교 해서 맞으면 맞다 틀리면 틀리다
-        if id == 'abc' and pw == '1234':
-            return "안녕하세요 ~ {} 님".format(id)
-        else:
-            return "아이디 또는 패스워드를 확인 하세요."
 
-@app.route('/form')
-def form():
-    return render_template('test.html')
 
-@app.route('/method', methods = ['GET', 'POST'])
-def method():
-    if request.method == 'GET':
-        return 'GET 으로 전송이다.'
-    else:
-        num = request.form['num']
-        name = request.form['name']
-        print(num,name)
-        dbdb.insert_data(num,name)
-        return'POST 이다. 학번은: {} 이름은: {}'.format(num,name)
+# # 로그아웃(session 제거)
+# @app.route('/logout')
+# def logout():
+#     session.pop('user', None)
+#     return redirect(url_for('form'))
+
+# # 로그인 사용자만 접근 가능으로 만들면
+# @app.route('/form')
+# def form():
+#     if 'user' in session:
+#         return render_template('test.html')
+#     return redirect(url_for('login'))
 
 @app.route('/getinfo')
 def getinfo():
@@ -114,13 +100,9 @@ def page_not_fount(error):
 @app.route('/img')
 def img():
     return render_template("image.html")
-    
-
-
-
 
 
 if __name__ == '__main__':
-    with app.test_request_context():
+    with app.test_request_context():
         print(url_for('daum'))
-    app.run(debug=True)
+	app.debug = True
